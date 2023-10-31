da2e2702304cd8a0ccd887a17f5fd7d0a3a04f45
PimehT <tareerepimeh@gmail.com>
2023-10-31 13:00:20 -0700

    5-print_comb2.py

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
+    elif i < 10:
+        print("0{}, ".format(i), end='')
+    else:
+        print("{}, ".format(i), end='')
