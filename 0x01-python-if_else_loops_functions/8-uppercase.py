470c45824d9cf2f001d245eef212d9e801efebef
PimehT <tareerepimeh@gmail.com>
2023-10-31 14:26:14 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        char = ord(str[i])
+        if char > 96 and char < 123:
+            char = char - 32
+        if len(str) - i == 1:
+            print("{}".format(chr(char)))
+        else:
+            print("{}".format(chr(char)), end='')
