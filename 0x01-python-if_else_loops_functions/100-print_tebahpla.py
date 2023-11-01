da97652450c81f5d09dd6ffcbe826c6815d49007
PimehT <tareerepimeh@gmail.com>
2023-10-31 17:25:41 -0700

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+i = 90
+def add_or_not(a):
+    if a % 2 == 0:
+        return a + 32
+    else:
+        return a
+while i > 64:
+    print("{}".format(chr(add_or_not(i))), end='')
+    i = i - 1
