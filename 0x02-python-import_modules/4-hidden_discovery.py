ac45ae0e63be2d941fbdc721eac2673e9bf9e4c5
PimehT <tareerepimeh@gmail.com>
2023-11-02 08:38:14 -0700

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+import hidden_4
+
+
+def print_cache():
+    cache = [item for item in dir(hidden_4) if "__" not in item]
+    for i in cache:
+        print("{}".format(i))
+
+
+print_cache()
