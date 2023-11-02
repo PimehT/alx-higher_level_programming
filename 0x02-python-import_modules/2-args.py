3cebeec545136d8f68af22d177b1f672ab5178cb
PimehT <tareerepimeh@gmail.com>
2023-11-02 07:31:24 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,11 @@
 import sys
 
 def list_out():
-    print("{} arguments:".format(len(sys.argv) - 1))
+    print("{} arguments".format(len(sys.argv) - 1), end='')
+    if len(sys.argv) == 1:
+        print(".")
+    else:
+        print(":")
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
 
