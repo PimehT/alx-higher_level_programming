f193ec92056f6f68689ea1669e384c3ad6389ed9
PimehT <tareerepimeh@gmail.com>
2023-11-06 07:06:04 -0800

    9-main.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    if not my_list:
+        return None
+    max_n = 0
+    for i in my_list:
+        if i > max_n:
+            max_n = i
+    return max_n
