5193b49121b9fbd9b40c2b93727fafca02299295
PimehT <tareerepimeh@gmail.com>
2023-11-06 07:00:06 -0800

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        result = (len(sentence), None)
+        return result
+    else:
+        result = (len(sentence), sentence[0])
+        return result
