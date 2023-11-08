680d5cf38f14fe5257a11af8b243e98243f5a3d2
PimehT <tareerepimeh@gmail.com>
2023-11-08 11:28:29 -0800

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or roman_string is None:
+        return 0
+    values = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+            }
+    total = 0
+    i = 0
+    while (i < len(roman_string)):
+        if values[roman_string[i]] < values[roman_string[i+1]]:
+            total += (values[roman_string[i+1]] - values[roman_string[i]])
+            i += 2
+        else:
+            total += values[roman_string[i]]
+            i += 1
+
+    return total
