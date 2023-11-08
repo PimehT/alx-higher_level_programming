ac8c50d8a63c75cba200d1df344d0eb31d858078
PimehT <tareerepimeh@gmail.com>
2023-11-08 11:47:44 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     if not roman_string or roman_string is None:
         return 0
+    if type(roman_string) != str:
+        return 0
     value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     i = 0
