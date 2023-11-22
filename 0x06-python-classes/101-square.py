ccae9c2aadb1560a5edb1a5111064580510a54d2
PimehT <tareerepimeh@gmail.com>
2023-11-22 13:56:31 -0800

    0x06-python-classes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -79,12 +79,12 @@ class Square:
                 print()
 
     def __str__(self):
-        """ print the square """
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__position[i]):
+        """Print the square."""
+        if self.__size != 0:
+            [print() for i in range(self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
+            if i != self.__size - 1:
                 print()
-            for i in range(self.__size):
-                print(" " * self.__position[], end="")
-                print("#" * self.__size)
+        return ""
