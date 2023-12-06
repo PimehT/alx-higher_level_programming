c5391e1e27429a8947e51312a3bc14708b3f7588
PimehT <tareerepimeh@gmail.com>
2023-12-06 15:29:02 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# 11-student.py
+""" defining a class called student """
+
+
+class Student:
+    """ class defined """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization of class Student
+        Args:
+        first_name (str): first name of student
+        last_name (str): last name of student
+        age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        student_attrs = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                student_attrs[attr] = getattr(self, attr)
+        return student_attrs
+
+    def reload_from_json(self, json):
+        return setattr(self, json)
