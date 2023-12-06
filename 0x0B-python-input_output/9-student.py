84cb3d397b5dedbcd4374a25cc182bcfdf81a7d9
PimehT <tareerepimeh@gmail.com>
2023-12-06 15:16:01 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 9-student.py
+""" defining a class called student """
+
+
+class Student:
+    """ class defined """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization of class Student
+        Args:
+        first_name (str): first name of student
+        last_name (str): last name of student
+        age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
