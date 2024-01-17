1f5b555b6d5f266fcf80decea72c765656f81040
PimehT <tareerepimeh@gmail.com>
2024-01-16 17:37:45 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,5 +3,6 @@
 SELECT `city`, AVG(`value`) AS `avg_tmp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
-GROUP BY `avg_tmp` DESC
+GROUP BY `city`
+ORDER BY `avg_tmp` DESC
 LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- displays the max temperature of each state (ordered by State name).
 SELECT MAX(`value`) AS `max_temp`
 FROM temperatures
-ORDER BY `state`
-LIMIT 3;
+GROUP BY `state`
+ORDER BY `state`;
