d60f43273ed191ba3350a25e5571b75e4d1a33c4
PimehT <tareerepimeh@gmail.com>
2024-02-12 23:03:27 +0100

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const strList = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-console.log(`${strList[0]}\n${strList[1]}\n${strList[2]}`);
+console.log(strList[0] + '\n' + strList[1] + '\n' + strList[2]);
