e6ee6de1f9a6d22ba525345d6632c036c1430728
PimehT <tareerepimeh@gmail.com>
2024-02-18 18:26:24 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+List all states with the 'search name given' from the table states
+in the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if len(sys.argv) != 5:
+    print("Usage: {} <username> <password> <database> <name>".format(sys.argv[0]))
+    sys.exit(1)
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+st_name = sys.argv[4]
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=database
+)
+
+cursor = db.cursor()
+query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+cursor.execute(query, (st_name,))
+results = cursor.fetchall()
+for row in results:
+    print("{}".format(row))
+
+cursor.close()
+db.close()
