4790229138f31382262e0de718b5e1c5e958e93e
PimehT <tareerepimeh@gmail.com>
2024-02-19 01:01:56 +0100

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+List all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    user_n = sys.argv[1]
+    pass_w = sys.argv[2]
+    data_b = sys.argv[3]
+    engine_str = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(
+        engine_str.format(user_n, pass_w, data_b), pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    new_state_name = "Louisiana"
+    new_state = State(name=new_state_name)
+    session.add(new_state)
+    session.commit()
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        if state.name == new_state_name:
+            print("{}".format(state.id))
+
+    session.close()
