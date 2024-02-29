c8b717b7f697aaeaa4d80cc1dd03fb0956561bd9
PimehT <tareerepimeh@gmail.com>
2024-02-29 23:40:46 +0100

    /0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/bash
 # get url and send http request to url
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL="$1"
-
 curl -sI $1 | grep -i Content-Length | cut -d ' ' -f 2
