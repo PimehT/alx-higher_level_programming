631ced314d38dca26f9ba7d56c16c2faff0f36db
PimehT <tareerepimeh@gmail.com>
2024-03-01 02:55:23 +0100

    /0x10-python-network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,22 +3,17 @@
 
 
 def find_peak(list_of_integers):
-    """
-    finds the peak value of a list of unsorted digits
-    must be done with lowest complexity
-    """
-    if len(list_of_integers) == 0:
+    if not list_of_integers:
         return None
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    elif len(list_of_integers) == 2:
-        return max(list_of_integers)
 
-    mid = len(list_of_integers) // 2
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
