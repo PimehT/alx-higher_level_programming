1f909bb6e1ce3e600e9ffd1dde8ccf749a07245d
PimehT <tareerepimeh@gmail.com>
2024-03-01 15:04:02 -0800

    0x11-python-network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,7 +11,8 @@ import sys
 def hbtn_header_req(url):
     """ prints value of X-Request-Id in response header of url"""
     r = requests.get(url)
-    print(r.headers['X-Request-Id'])
+    data = r.headers['X-Request-Id']
+    print(data)
 
 
 if __name__ == "__main__":
