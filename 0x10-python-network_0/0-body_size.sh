0e7aa9a89808c5cc4be853b3126853ded1966194
PimehT <tareerepimeh@gmail.com>
2024-03-01 01:20:26 +0100

    /0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # get url and send http request to url
+
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends a DELETE request to the URL passed and displays the body of th response
+
 curl -sX DELETE "$1"
