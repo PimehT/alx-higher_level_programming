960708a15561f0af2514e0d93ac5811405486111
PimehT <tareerepimeh@gmail.com>
2024-03-01 01:56:47 +0100

    /0x10-python-network_0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a post request to the passed URL, and displays the body of the message
-curl -sX POST -d "email=test%40gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -sX POST --data-urlencode "email=test@gmail.com" --data-urlencode "subject=I will always be here for PLD" "$1"
