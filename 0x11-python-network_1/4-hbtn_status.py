474bdba200e4f0844823b719d29bb8be3b01bc54
PimehT <tareerepimeh@gmail.com>
2024-03-01 14:40:42 -0800

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,7 +10,7 @@ def hbtn_status_req():
     """using `requests` package to fetch url"""
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}".format(type(r.text)))
-    print("\tcontent: {}".format(r.text))
+    print("\t- content: {}".format(r.text))
 
 
 if __name__ == "__main__":
