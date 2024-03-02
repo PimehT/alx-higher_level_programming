cd1d4397c9a961b8f10e7d6346f1d91b86e29e9f
PimehT <tareerepimeh@gmail.com>
2024-03-01 16:19:05 -0800

    0x11-python-network_1

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that sends POST request with a letter as arg
+if no letter, send empty arg
+if json format is good, display '[<id>] <name>'
+Otherwise:
+    display 'Not a valid JSON' if invalid or
+    display 'No result' if empty
+"""
+import requests
+import sys
+
+
+def json_api(char=None):
+    """ return api search in json format """
+    data = {}
+    if char is None:
+        data['q'] = ""
+    else:
+        data['q'] = char
+
+    try:
+        r = requests.post('http://0.0.0.0:5000/search_user', data)
+        user = r.json()
+        if not user:
+            print("No result")
+        else:
+            print("[{}] {}".format(user['id'], user['name']))
+    except JSONDecodeError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        char = None
+    else:
+        char = sys.argv[1]
+    json_api(char)
