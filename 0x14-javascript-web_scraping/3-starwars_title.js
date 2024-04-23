cb43b2b7aea2bb17b34f96e06552b5a44c7d723e
PimehT <tareerepimeh@gmail.com>
2024-04-23 23:45:12 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Status:', response.statusCode);
-    } else {
-        const movie = JSON.parse(body);
-        console.log(movie.title);
-    }
+request(url, (err, resp, body) => {
+  if (err) {
+    console.log(err);
+  } else if (resp.statusCode !== 200) {
+    console.log(`Status: ${resp.statusCode}`);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
 });
