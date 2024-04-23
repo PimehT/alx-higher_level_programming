68699854279bc672a733f7b59339790f1a10b8dc
PimehT <tareerepimeh@gmail.com>
2024-04-23 23:03:35 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,10 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-const content = process.argv[3];
+const fileData = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-        console.error(err);
-    }
+fs.writeFile(filePath, fileData, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
 });
