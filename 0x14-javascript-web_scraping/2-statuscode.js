259b40367a1014c8e33684061af8aa80d19cc410
PimehT <tareerepimeh@gmail.com>
2024-04-23 23:14:23 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,10 +3,7 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request(url, function(error, response) {
-    if (error) {
-        console.error('Error:', error);
-    } else {
-        console.log('code:', response.statusCode);
-    }
+request(url, (err, resp) => {
+  if (err) console.log(err);
+  else console.log(`code: ${resp.statusCode}`);
 });
