2f83a0ec980f90e8ed9954a734ce20bac40bfb0f
PimehT <tareerepimeh@gmail.com>
2024-04-24 02:12:10 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
 
-const apiUrl = process.argv[2];
+const url = process.argv[2];
 
-request(apiUrl, function (error, response, body) {
-    if (error) {
-        console.error('Error:', error);
-    } else {
-        const tasks = JSON.parse(body);
-        const completedTasksByUser = {};
+request(url, (err, resp, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const tasks = JSON.parse(body);
+    const completedTasksByUser = {};
 
-        tasks.forEach(function (task) {
-            if (task.completed) {
-                if (completedTasksByUser[task.userId]) {
-                    completedTasksByUser[task.userId]++;
-                } else {
-                    completedTasksByUser[task.userId] = 1;
-                }
-            }
-        });
+    tasks.forEach(function (task) {
+      if (task.completed) {
+        if (completedTasksByUser[task.userId]) {
+          completedTasksByUser[task.userId]++;
+        } else {
+          completedTasksByUser[task.userId] = 1;
+        }
+      }
+    });
 
-        console.log(completedTasksByUser);
-    }
+    console.log(completedTasksByUser);
+  }
 });
