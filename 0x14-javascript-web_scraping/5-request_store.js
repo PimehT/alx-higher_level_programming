1366dba688ca721ebdeefd481a3a95e681acd21a
PimehT <tareerepimeh@gmail.com>
2024-04-24 01:04:23 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,18 +5,14 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Status:', response.statusCode);
-    } else {
-        fs.writeFile(filePath, body, 'utf8', (error) => {
-            if (error) {
-                console.error('Error:', error);
-            } else {
-                console.log('File saved successfully!');
-            }
-        });
-    }
+request(url, (err, resp, body) => {
+  if (err) {
+    console.log(err);
+  } else if (resp.statusCode !== 200) {
+    console.log(`Status: ${resp.statusCode}`);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) console.log(err);
+    });
+  }
 });
