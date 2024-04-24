998903d47ba75474f8a2d39f4a7371315ff54520
PimehT <tareerepimeh@gmail.com>
2024-04-24 02:42:01 +0100

    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const baseUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+          if (error) {
+              reject(error);
+          } else {
+              resolve(JSON.parse(body));
+          }
+      });
+  });
+}
+
+fetchData(baseUrl)
+  .then((movie) => {
+    const characters = movie.characters;
+    characters.forEach(async (char) => {
+      try {
+        const character = await fetchData(char);
+        console.log(character.name);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
