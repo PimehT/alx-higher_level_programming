8e24f7554c996395cd6b819ee2a3bcd03722fe11
PimehT <tareerepimeh@gmail.com>
2024-05-05 09:08:41 +0100

    fixed task 2 and 3

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,8 +1,3 @@
-const clicker = document.querySelector('#red_header');
-
-clicker.addEventListener('click', changeColor);
-
-function changeColor () {
-  const header = document.querySelector('header');
-  header.style.color = '#FF0000';
-}
+$('#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+});
