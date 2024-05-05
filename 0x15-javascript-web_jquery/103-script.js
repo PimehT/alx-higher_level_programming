a7b566d9b71cb0f376a1058b4a300d94066267e6
PimehT <tareerepimeh@gmail.com>
2024-05-05 09:37:07 +0100

    made task 103 better

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,19 +1,17 @@
 $(document).ready(function () {
-  $('#btn_translate').click(function () {
+  $('#btn_translate').click(translateHello);
+
+  $('#language_code').keypress(function (event) {
+    if (event.keyCode === 13) {
+      translateHello();
+    }
+  });
+
+  function translateHello () {
     const languageCode = $('#language_code').val();
     const helloLink = `https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`;
     $.get(helloLink, function (data) {
       $('#hello').text(data.hello);
     });
-  });
-
-  $('#language_code').keypress(function (event) {
-    if (event.keyCode === 13) {
-      const languageCode = $('#language_code').val();
-      const helloLink = `https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`;
-      $.get(helloLink, function (data) {
-        $('#hello').text(data.hello);
-      });
-    }
-  });
+  }
 });
