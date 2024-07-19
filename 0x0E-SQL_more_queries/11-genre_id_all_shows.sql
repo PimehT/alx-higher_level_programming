0b4dfda016d6a3548c49e12821f9ff893b5fd5bc
PimehT <tareerepimeh@gmail.com>
2024-07-19 18:52:18 +0100

    task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Script that lists all shows contained in the database hbtn_0d_tvshows
+-- Each record display: `tv_shows.title` - `tv_show_genres.genre_id`
+-- If show has no genre, display NULL
+
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_show_genres RIGHT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
