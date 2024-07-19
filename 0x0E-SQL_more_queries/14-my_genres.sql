bb4af6da3e4cd6d06b7102afa4955da3771e8b4b
PimehT <tareerepimeh@gmail.com>
2024-07-19 23:30:56 +0100

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,8 @@
 -- Result sorted in ascending order by the genre name
 
 SELECT tv_genres.name AS name
-FROM tv_show_genres NATURAL JOIN tv_genres NATURAL JOIN tv_shows
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_shows.name = 'Dexter'
+FROM tv_show_genres
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
