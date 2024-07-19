d6ca524d6e04e413cda2525e891d21418581cb55
PimehT <tareerepimeh@gmail.com>
2024-07-19 23:11:42 +0100

    task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Script that lists all genres from hbtn_0d_tvshows linked to the show `Dexter`
+-- Display: tv_genres.name
+-- Result sorted in ascending order by the genre name
+
+SELECT tv_genres.name AS name
+FROM tv_show_genres JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id = 8
+ORDER BY tv_genres.name ASC;
