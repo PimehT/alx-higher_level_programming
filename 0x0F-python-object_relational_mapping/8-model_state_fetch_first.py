2365e60006382506d9c212c65919badc64d310f4
PimehT <tareerepimeh@gmail.com>
2024-07-20 01:34:51 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,28 +2,23 @@
 """
 List first State object from the database hbtn_0e_6_usa
 """
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
 from model_state import Base, State
-import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
 
 if __name__ == "__main__":
-    user_n = sys.argv[1]
-    pass_w = sys.argv[2]
-    data_b = sys.argv[3]
-    engine_str = 'mysql+mysqldb://{}:{}@localhost/{}'
-    engine = create_engine(
-        engine_str.format(user_n, pass_w, data_b), pool_pre_ping=True
-    )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    state = session.query(State).order_by(State.id).first()
-
-    if (state):
-        print("{}: {}".format(state.id, state.name))
+    row = session.query(State).order_by(State.id).first()
+    if row:
+        print(f"{row.id}: {row.name}")
     else:
         print('Nothing')
-
     session.close()
